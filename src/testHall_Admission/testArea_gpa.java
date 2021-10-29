2c64f51046e690673e6f043c32b210e24d78ff2a
TCSiu <amble3232@gmail.com>
2021-10-30 00:11:17 +0800

    updagte test gpa
    convert score all test cases cause failure

---
+++ src/testHall_Admission/testArea_gpa.java
@@ -0,0 +1,50 @@
+package testHall_Admission;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import HallAdmissionSystem.Area_ECA;
+
+class testArea_gpa {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/testResource/testCreateArea_gpa.csv")
+	void testConvertScore(String input,boolean expectedFail,int expectedResult,String msg) {
+		int actualResult = 0;
+		boolean actualFail = false;
+		
+		try {
+			actualResult = Area_ECA.convertScore(input);
+		}catch(NumberFormatException e) {
+			actualFail = true;
+		}
+		assertEquals(expectedFail,actualFail,msg);
+		if(!expectedFail) {
+			assertEquals(expectedResult,actualResult,msg);
+		}
+	}
+
+}
