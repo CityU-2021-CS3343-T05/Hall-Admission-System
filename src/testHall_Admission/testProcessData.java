6493cee82f142abe85a5b4ae4a7894d6085b2d9e
TCSiu <amble3232@gmail.com>
2021-11-01 11:01:00 +0800

    update test process data

--- src/testHall_Admission/testProcessData.java
+++ src/testHall_Admission/testProcessData.java
@@ -23,10 +23,9 @@ class testProcessData {
 	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 	Hall hall1;
 	ProcessData actualResult;
-	Student std1 , std2;
-	Application application1 = null , application2 = null;
-	Date expectedDate1 = null;
-	Date expectedDate2 = null;
+	Student std1, std2, std3, std4;
+	Application application1 = null, application2 = null, application3 = null, application4 = null;
+	Date expectedDate1 = null, expectedDate2 = null, expectedDate3 = null, expectedDate4 = null;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -49,8 +48,8 @@ class testProcessData {
 		fieldRejectedList.set(fieldRejectedList,new PriorityQueue<>(new ApplicationDateCompare()));
 		std1 = new Student("S00001", "Jennifer", "Passw0rd", false, 4);
 		std2 = new Student("S00002", "Elston", "Passw0rd", false, 2);
-		expectedDate1 = null;
-		expectedDate2 = null;
+		std3 = new Student("S00003", "Wilson", "Passw0rd", false, 2);
+		std4 = new Student("S00004", "Chung", "Passw0rd", false, 2);
 	}
 
 	@AfterEach
@@ -85,7 +84,7 @@ class testProcessData {
 		
 		if(!expectedFail) {
 			actualResult.addToList(application1);
-			String expectedOutput="full        \tHall 1\tfalse\n" + expectedDate1+ "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0" ;
+			String expectedOutput = "full        \tHall 1\tfalse\n" + expectedDate1+ "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0" ;
 			System.out.print(actualResult.toString());
 			assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
 		}
@@ -98,15 +97,15 @@ class testProcessData {
 		
 		String msg = "Test empty waiting list and empty rejected list";
 		
-		String expectedOutput="Waiting List\nEmpty listing\n\nReject List\nEmpty listing";
+		String expectedOutput = "Waiting List\nEmpty listing\n\nReject List\nEmpty listing";
 		System.out.print(ProcessData.getListing());
-		assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
+		assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
 	}
 	
 	@Test
 	void testGetListing2() {
 		
-		String msg = "Test non-empty waiting list and empty rejected list";
+		String msg = "Test 1 application in waiting list and empty rejected list";
 		
 		boolean expectedFail = false;
 		boolean actualFail = false;
@@ -120,9 +119,9 @@ class testProcessData {
 		
 		if(!expectedFail) {
 			ProcessData.addToWaiting(application1);
-			String expectedOutput="Waiting List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n\nReject List\nEmpty listing";
+			String expectedOutput = "Waiting List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n\nReject List\nEmpty listing";
 			System.out.print(ProcessData.getListing());
-			assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
 		}
 		assertEquals(expectedFail,actualFail,msg);
 	}
@@ -130,7 +129,7 @@ class testProcessData {
 	@Test
 	void testGetListing3() {
 		
-		String msg = "Test empty waiting list and non-empty rejected list";
+		String msg = "Test empty waiting list and 1 application in rejected list";
 		
 		boolean expectedFail = false;
 		boolean actualFail = false;
@@ -144,9 +143,9 @@ class testProcessData {
 		
 		if(!expectedFail) {
 			ProcessData.addToReject(application1);
-			String expectedOutput="Waiting List\nEmpty listing\n\nReject List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0";
+			String expectedOutput = "Waiting List\nEmpty listing\n\nReject List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0";
 			System.out.print(ProcessData.getListing());
-			assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
 		}
 		assertEquals(expectedFail,actualFail,msg);
 	}
@@ -154,7 +153,7 @@ class testProcessData {
 	@Test
 	void testGetListing4() {
 		
-		String msg = "Test non-empty waiting list and non-empty rejected list";
+		String msg = "Test 1 application in waiting list and 1 application in rejected list";
 		
 		boolean expectedFail = false;
 		boolean actualFail = false;
@@ -172,9 +171,221 @@ class testProcessData {
 		if(!expectedFail) {
 			ProcessData.addToWaiting(application1);
 			ProcessData.addToReject(application2);
-			String expectedOutput="Waiting List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n\nReject List\n" + expectedDate2 + "\tS00002\tfalse\t2\tHall 1\t8\t4\t10\t10\t0";
+			String expectedOutput = "Waiting List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n\nReject List\n" + expectedDate2 + "\tS00002\tfalse\t2\tHall 1\t8\t4\t10\t10\t0";
 			System.out.print(ProcessData.getListing());
-			assertEquals(expectedOutput,outputStreamCaptor.toString().trim());
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetListing5() {
+		
+		String msg = "Test 2 applications in waiting list and empty rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToWaiting(application1);
+			ProcessData.addToWaiting(application2);
+			String expectedOutput = "Waiting List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n"+ expectedDate2 + "\tS00002\tfalse\t2\tHall 1\t8\t4\t10\t10\t0\n\nReject List\nEmpty listing";
+			System.out.print(ProcessData.getListing());
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetListing6() {
+		
+		String msg = "Test empty waiting list and 2 applications in rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToReject(application1);
+			ProcessData.addToReject(application2);
+			String expectedOutput = "Waiting List\nEmpty listing\n\nReject List\n" + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0\n"+ expectedDate2 + "\tS00002\tfalse\t2\tHall 1\t8\t4\t10\t10\t0";
+			System.out.print(ProcessData.getListing());
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetSpecificListing1() {
+		
+		String msg = "Test getting application in empty waiting list and rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			String expectedOutput = "null";
+			System.out.print(ProcessData.getSpecificListing(application1));
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+		
+	}
+	
+	@Test
+	void testGetSpecificListing2() {
+		
+		String msg = "Test getting application in non-empty waiting list and rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 application3 = new Application(std3, 1, new ScoreComponent("GPA", "4.0", "2", "300", "Cheung Chau"));
+			 expectedDate3 = new Date();
+			 application4 = new Application(std4, 1, new ScoreComponent("GPA", "2.0", "2", "300", "Cheung Chau"));
+			 expectedDate4 = new Date();
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToWaiting(application1);
+			ProcessData.addToWaiting(application2);
+			ProcessData.addToReject(application3);
+			ProcessData.addToReject(application4);
+			String expectedOutput = "Waiting List: " + expectedDate1 + "\tS00001\tfalse\t4\tHall 1\t10\t4\t10\t10\t0";
+			System.out.print(ProcessData.getSpecificListing(application1));
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetSpecificListing3() {
+		
+		String msg = "Test getting application in non-empty waiting list and rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 application3 = new Application(std3, 1, new ScoreComponent("GPA", "4.0", "2", "300", "Cheung Chau"));
+			 expectedDate3 = new Date();
+			 application4 = new Application(std4, 1, new ScoreComponent("GPA", "2.0", "2", "300", "Cheung Chau"));
+			 expectedDate4 = new Date();
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToWaiting(application1);
+			ProcessData.addToWaiting(application2);
+			ProcessData.addToReject(application3);
+			ProcessData.addToReject(application4);
+			String expectedOutput = "Waiting List: " + expectedDate2 + "\tS00002\tfalse\t2\tHall 1\t8\t4\t10\t10\t0";
+			System.out.print(ProcessData.getSpecificListing(application2));
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetSpecificListing4() {
+		
+		String msg = "Test getting application in non-empty waiting list and rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 application3 = new Application(std3, 1, new ScoreComponent("GPA", "4.0", "2", "300", "Cheung Chau"));
+			 expectedDate3 = new Date();
+			 application4 = new Application(std4, 1, new ScoreComponent("GPA", "2.0", "2", "300", "Cheung Chau"));
+			 expectedDate4 = new Date();
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToWaiting(application1);
+			ProcessData.addToWaiting(application2);
+			ProcessData.addToReject(application3);
+			ProcessData.addToReject(application4);
+			String expectedOutput = "Rejected List: " + expectedDate3 + "\tS00003\tfalse\t2\tHall 1\t9\t4\t10\t10\t0";
+			System.out.print(ProcessData.getSpecificListing(application3));
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
+		}
+		assertEquals(expectedFail,actualFail,msg);
+	}
+	
+	@Test
+	void testGetSpecificListing5() {
+		
+		String msg = "Test getting application in non-empty waiting list and rejected list";
+		
+		boolean expectedFail = false;
+		boolean actualFail = false;
+		
+		try {
+			 application1 = new Application(std1, 1, new ScoreComponent("GPA", "4.3", "2", "300", "Cheung Chau"));
+			 expectedDate1 = new Date();
+			 application2 = new Application(std2, 1, new ScoreComponent("GPA", "3.3", "2", "300", "Cheung Chau"));
+			 expectedDate2 = new Date();
+			 application3 = new Application(std3, 1, new ScoreComponent("GPA", "4.0", "2", "300", "Cheung Chau"));
+			 expectedDate3 = new Date();
+			 application4 = new Application(std4, 1, new ScoreComponent("GPA", "2.0", "2", "300", "Cheung Chau"));
+			 expectedDate4 = new Date();
+		} catch (Ex_WrongExamType e) {
+			actualFail = true;
+		}
+		
+		if(!expectedFail) {
+			ProcessData.addToWaiting(application1);
+			ProcessData.addToWaiting(application2);
+			ProcessData.addToReject(application3);
+			ProcessData.addToReject(application4);
+			String expectedOutput = "Rejected List: " + expectedDate4 + "\tS00004\tfalse\t2\tHall 1\t5\t4\t10\t10\t0";
+			System.out.print(ProcessData.getSpecificListing(application4));
+			assertEquals(expectedOutput,outputStreamCaptor.toString().trim(),msg);
 		}
 		assertEquals(expectedFail,actualFail,msg);
 	}
